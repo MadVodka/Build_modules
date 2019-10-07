cc37955a3f1983e8f1d04fe1a3e6fee3e4d6959c
Ivan_Vatlin <ivan_valtin@epam.com>
2019-10-07 17:22:47 +0400

    Initial commit

--- xml_json_data/src/main/java/ivan/vatlin/data/Application.java
+++
@@ -1,8 +0,0 @@
-package ivan.vatlin.data;
-
-public class Application {
-    public static void main(String[] args) {
-        XmlDtoApplication.run();
-        JsonXmlApplication.run();
-    }
-}
--- xml_json_implementation/src/main/java/ivan/vatlin/xml_json_implementation/exceptions/XmlValidationException.java
+++
@@ -1,7 +0,0 @@
-package ivan.vatlin.xml_json_implementation.exceptions;
-
-public class XmlValidationException extends Exception {
-    public XmlValidationException(String message) {
-        super(message);
-    }
-}
